f381c2639fc8919cacd5b16adfe4b43dec80c16d
wsdt <kevin.riedl.privat@gmail.com>
2025-07-08 17:13:14 +0200

    fix: conf

--- frontend/src/config/snap.ts
+++ frontend/src/config/snap.ts
@@ -15,3 +15,9 @@ export const snapPackageVersion = import.meta.env.VITE_SNAP_VERSION ?? "1.1.22";
 
 /* Contract address that you want to invoke. */
 export const YOUR_CONTRACT = import.meta.env.VITE_SMART_CONTRACT;
+
+console.log('Config', {
+    orig: defaultSnapOrigin,
+    snapVersion: snapPackageVersion,
+    contr: YOUR_CONTRACT
+})
