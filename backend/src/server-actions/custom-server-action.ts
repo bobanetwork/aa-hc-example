e74ce7528c615c2c0e1d27de37e7cee853fb8c0c
wsdt <kevin.riedl.privat@gmail.com>
2025-12-17 11:19:23 +0100

    fix: reset hex selector

--- backend/src/server-actions/custom-server-action.ts
+++ backend/src/server-actions/custom-server-action.ts
@@ -34,7 +34,7 @@ const generateResponseV7 = async (
     respPayload: any,
 ): Promise<ServerActionResponse> => {
     function selectorHex(name: string): string {
-        const hex = keccak256(toHex(name));
+        const hex = toHex(keccak256(name as `0x${string}`));
         return hex.slice(0, 10);
     }
     if (
